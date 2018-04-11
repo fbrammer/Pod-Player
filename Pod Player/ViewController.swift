96620fa08793481ccdd83fa3bd7a84b6ad35fd28
Frank Brammer <fbrammer@brammeris.com>
2018-04-11 09:44:54 -0400

    Initial Commit.  No code added yet.

---
+++ Pod Player/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Pod Player
+//
+//  Created by Frank Brammer on 4/11/18.
+//  Copyright © 2018 BrammerIS. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
