09d06b118ba43e2e32302117a4a0926a7f01e630
Frank Brammer <fbrammer@brammeris.com>
2018-04-11 19:18:15 -0400

    Setting up the first panel: Podcast

---
+++ Pod Player/PodCastViewController.swift
@@ -0,0 +1,22 @@
+//
+//  PodCastViewController.swift
+//  Pod Player
+//
+//  Created by Frank Brammer on 4/11/18.
+//  Copyright © 2018 BrammerIS. All rights reserved.
+//
+
+import Cocoa
+
+class PodCastViewController: NSViewController {
+
+    @IBOutlet weak var podcastURLTextField: NSTextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+    @IBAction func addPodCastClicked(_ sender: Any) {
+    }
+}
